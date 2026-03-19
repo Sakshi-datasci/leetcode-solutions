3ed765507f7987d1d8341577c9b15b50b0a379d1
Sakshi kumari <sakshi.good787@gmail.com>
2026-03-19 22:28:12 +0530

    Add Solution class for Alice's digit game

---
+++ Easy/3232_digit_game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canAliceWin(self, nums: list[int]) -> bool:
+        single_sum = 0
+        double_sum = 0
+
+        for num in nums:
+            if num < 10:
+                single_sum += num
+            else:
+                double_sum += num
+
+        # Check both choices
+        return single_sum > double_sum or double_sum > single_sum
